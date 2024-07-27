0f8a265a164b2b2ceaff5ccc500cd73245bf1db2
Noble Benjamin <nobleben2008@gmail.com>
2024-07-27 21:25:58 +0100

    Added the pages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
