d18cfba68588998dc460156dd8a5af15a0ce68ba
Noble Benjamin <nobleben2008@gmail.com>
2024-07-27 21:29:05 +0100

    Implemented the basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </BrowserRouter>
   )
