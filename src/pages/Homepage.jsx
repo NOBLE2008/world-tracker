cf49f5e9a09c2444b22c0b8dbbe946a1bb124bb4
Noble Benjamin <nobleben2008@gmail.com>
2024-07-27 21:38:20 +0100

    Used the Link comp for the first time

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Homepage() {
   return (
     <div>
       <h1>Homepage</h1>
+      <Link to={'/pricing'}>Pricing</Link>
     </div>
   )
 }
