0c0c63a66217dc8f34b41577e6f8e012f6a511df
Noble Benjamin <nobleben2008@gmail.com>
2024-07-27 21:32:30 +0100

    Added a Not Found

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
